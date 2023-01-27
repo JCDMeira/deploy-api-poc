f8c2b2520c4edcadfba67eea612b88aa586ce3a5
Jean Meira <jean.meira10@hotmail.com>
2023-01-26 21:19:10 -0300

    feat: initial setup

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const routes = express.Router();
+
+routes.get("/teste", (req, res) =>
+  res.status(200).json({ message: "hello world" })
+);
+
+app.use(routes);
+
+app.listen("3333", () => console.log("online"));
