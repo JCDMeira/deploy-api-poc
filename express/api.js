00e0034189bed6f9b848f7928ee18ed537a33932
Jean Meira <jean.meira10@hotmail.com>
2023-01-31 21:26:44 -0300

    build: test#25

--- express/api.js
+++ express/api.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", (req, res) => res.status(200).json({ message: "Hello world" }));
-
 app.use("/api/teste", (req, res) =>
   res.status(201).json({ message: "testando" })
 );
+
+app.use("/api", (req, res) => res.status(200).json({ message: "Hello world" }));
 
 export default app;
 export const handler = servless(app);
